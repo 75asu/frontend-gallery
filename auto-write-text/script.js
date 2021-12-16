67e890b1109f49da253c4ddbd47221c33d6db259
meAsutosh <78539272+meAsutosh@users.noreply.github.com>
2021-12-16 14:00:07 +0530

    added the auto-write project

---
+++ auto-write-text/script.js
@@ -0,0 +1,15 @@
+const text = "Nothing important, dropped this message here to waste few seconds of your life while you are reading it       ...";
+
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if (index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
